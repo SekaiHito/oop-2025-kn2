628cdf41f519b85abd0d206ad44f35f013df031d
agelesslove <romantol873@gmail.com>
2025-04-01 14:36:33 +0300

    кебаб

--- lesiv-roman/string-formatting.py
+++
@@ -1,3 +0,0 @@
-data = ("John", "Doe", 53.44)
-format_string = "Hello %s %s. Your current balance is $%s." #%s — означає, що на його місце буде підставлено значення у вигляді рядка.
-print(format_string % data)
