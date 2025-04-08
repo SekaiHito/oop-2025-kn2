6c084104d84eb1282ecf75b7834c84b20f1b6cb8
RomanFdn <fedunroman43@gmail.com>
2025-04-08 10:09:10 +0300

    Refactor README and Python variable examples for clarity and consistency

--- fedun-roman/lab-2/variables-and-types.py
+++ fedun-roman/lab-2/variables-and-types.py
@@ -1,22 +1,17 @@
-# Це змінна з типом int (ціле число)
+#Змінна int (ціле число)
 age = 18
 
-# Це змінна з типом float (десяткове число)
+#Змінна float (десяткове число)
 height = 1.89
 
-# Це змінна з типом str (рядок)
+#Змінна str (рядок)
 name = "Fdn"
 
-# Це змінна з типом bool (логічне значення)
-is_student = True
-
-# Виводимо значення та тип кожної змінної
+#Значення для змінних
 print("Ім'я:", name)
 print("Вік:", age)
 print("Зріст:", height, "м")
-print("Студент:", is_student)
 
 print("Тип змінної 'age':", type(age))
 print("Тип змінної 'height':", type(height))
 print("Тип змінної 'name':", type(name))
-print("Тип змінної 'is_student':", type(is_student))
