740b499eb20ca0c904584ecf39f89161d8255579
Volodymyr Skalskyi <volodymyr.skalskyi@uipath.com>
2025-04-14 18:29:02 +0300

    lab

--- volodymyr-skalskyi/lab3/complex-conditions.py
+++ volodymyr-skalskyi/lab3/complex-conditions.py
@@ -13,22 +13,6 @@ if number % 2 == 0:
 else:
     properties.append("непарне")
 
-# Перевірка на додатність
-if number > 0:
-    properties.append("додатне")
-elif number < 0:
-    properties.append("від'ємне")
-else:
-    properties.append("нуль")
-
-# Перевірка на ділення на 3
-if number % 3 == 0:
-    properties.append("ділиться на 3")
-
-# Перевірка на ділення на 5
-if number % 5 == 0:
-    properties.append("ділиться на 5")
-
 # Перевірка на діапазон
 if 0 <= number <= 100:
     properties.append("в діапазоні від 0 до 100")
@@ -42,18 +26,4 @@ print(f"\nЧисло {number} є:")
 for property in properties:
     print(f"- {property}")
 
-# Додаткова перевірка складних умов
-print("\nДодаткові перевірки:")
-
-# Перевірка на ділення на 3 або 5
-if number % 3 == 0 or number % 5 == 0:
-    print("- Ділиться на 3 або на 5")
-
-# Перевірка на ділення на 3 і 5
-if number % 3 == 0 and number % 5 == 0:
-    print("- Ділиться і на 3, і на 5")
-
-# Перевірка на діапазон і парність
-if 0 <= number <= 100 and number % 2 == 0:
-    print("- Парне число в діапазоні від 0 до 100")
         
