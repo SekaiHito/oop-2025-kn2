8e22e51b74bad8ce1db63d2cf8ac4fe04cec341f
SekaiHito <maksv5476@gmail.com>
2025-04-29 10:11:36 +0300

    заготовка звіту

--- volodymyr-maksymiv/lab6/random.py
+++ volodymyr-maksymiv/lab6/random.py
@@ -1,4 +1,8 @@
 class Alfa:
-    
-    def __init__(self,name,lname,pet,car):
-        pass
+    pay_raise = 1.02
+    def __init__(self,name,lname,pay,mail):
+        self.name = name
+        self.name = name
+        self.name = name
+        self.name = name
+        
