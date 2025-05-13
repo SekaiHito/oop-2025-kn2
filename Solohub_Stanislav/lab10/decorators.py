dd5cb2622d7258da7d828ceb5c8e7c5c5ac10454
SolohubStanislav <solohubsv@lnup.edu.ua>
2025-05-13 15:42:55 +0300

    Decorators

--- Solohub_Stanislav/lab10/decorators.py
+++ Solohub_Stanislav/lab10/decorators.py
@@ -1,13 +1,11 @@
 class Circle:
     def __init__(self, radius):
-        self._radius = radius  # захищене поле
+        self._radius = radius  
 
-    # Getter — декоратор @property
     @property
     def radius(self):
         return self._radius
 
-    # Setter — перевіряє, що радіус додатній
     @radius.setter
     def radius(self, value):
         if value > 0:
@@ -15,25 +13,20 @@ class Circle:
         else:
             raise ValueError("Радіус має бути додатним числом.")
 
-    # Deleter — дозволяє видалити радіус
     @radius.deleter
     def radius(self):
         print("Радіус видалено.")
         del self._radius
 
-    # Властивість — площа кола (тільки для читання)
     @property
     def area(self):
         from math import pi
         return pi * self._radius ** 2
 
-    # Властивість — діаметр кола (тільки для читання)
     @property
     def diameter(self):
         return self._radius * 2
 
-
-# Демонстрація використання декораторів
 def main():
     c = Circle(5)
     print(f"Початковий радіус: {c.radius}")
