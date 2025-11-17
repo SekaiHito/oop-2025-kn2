490fff6507d44e077264ec33a6925dac515ec7eb
SekaiHito <maksv5476@gmail.com>
2025-11-17 10:09:34 +0200

    minesweeper 1.0

--- volodymyr-maksymiv/minesweeper 1.0/cell.py
+++ volodymyr-maksymiv/minesweeper 1.0/cell.py
@@ -1,28 +1,27 @@
-from tkinter import Button, Label, messagebox
+from tkinter import Button, Label, messagebox, SUNKEN
 import random
-from window import Window
-import sys
 
 class Cell:
     all = []
     cell_count_label_object = None
     cell_count = 0
-    def __init__(self, x, y, is_mine=False):
+    
+    def __init__(self, x, y, game, is_mine=False): 
         self.is_mine = is_mine
         self.is_opened = False
         self.is_mine_candidate = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
+        self.game = game 
         
         Cell.all.append(self)
-
 
     def create_btn_oblect(self, location):
         btn = Button(
             location,
-            width=8,
-            height=4,
+            width=5,
+            height=3,
         )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
@@ -30,19 +29,21 @@ class Cell:
     
     
     def left_click_actions(self, event):
-        if self.is_mine:
-            self.show_mine()
-        else:
-            if self.surrounded_cells_mines_lens == 0:
-                for cell_obj in self.surrounded_cells:
-                    cell_obj.show_cell()
-            self.show_cell()
-        if Cell.cell_count == 0:
-            messagebox.showinfo('Game over!', 'Congratulation!')
+        
+        if self.game.first_click:
+            self.game.initialize_mines(self)
 
-        self.cell_btn_object.unbind('<Button-1>')
-        self.cell_btn_object.unbind('<Button-3>')
-    
+        if self.is_opened or self.is_mine_candidate or self.game.game_over:
+            return 
+
+        if self.is_mine:
+            self.game.game_over_lose() 
+        else:
+            self.show_cell() 
+            
+            if Cell.cell_count == 0:
+                self.game.game_over_win()
+
     def get_cell_by_axis(self, x, y):
         for cell in Cell.all:
             if cell.x == x and cell.y == y:
@@ -69,28 +70,38 @@ class Cell:
         for cell in self.surrounded_cells:
             if cell.is_mine:
                 counter += 1
-
         return counter
 
     def show_cell(self):
-        if not self.is_opened:
-            Cell.cell_count -= 1
-            self.cell_btn_object.configure(text=self.surrounded_cells_mines_lens)
-            if Cell.cell_count_label_object:
-                Cell.cell_count_label_object.configure(
-                    text = f"Cells left:{Cell.cell_count}"
-                )
-            default_bg = Button().cget("background")
-            self.cell_btn_object.configure(bg=default_bg)
-        self.is_opened = True
+        if self.is_opened:
+            return
 
-    
-    def show_mine(self):
-        self.cell_btn_object.configure(bg="red")
-        messagebox.showinfo('Game over!', 'You clicked on a mine!')
-        sys.exit()
+        self.is_opened = True
+        Cell.cell_count -= 1
+        
+        mines_nearby = self.surrounded_cells_mines_lens
+        self.cell_btn_object.configure(text=str(mines_nearby) if mines_nearby > 0 else "")
+        
+        if Cell.cell_count_label_object:
+            Cell.cell_count_label_object.configure(
+                text = f"Cells left:{Cell.cell_count}"
+            )
+        
+        default_bg = Button().cget("background")
+        self.cell_btn_object.configure(bg=default_bg, relief=SUNKEN) 
+
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
+
+        if mines_nearby == 0:
+            for cell in self.surrounded_cells:
+                if not cell.is_opened and not cell.is_mine_candidate:
+                    cell.show_cell()
 
     def right_click_actions(self, event):
+        if self.is_opened or self.game.game_over:
+            return
+
         if not self.is_mine_candidate:
             self.cell_btn_object.configure(
                 bg = 'orange'
@@ -103,6 +114,3 @@ class Cell:
 
     def __repr__(self):
         return f"Cell({self.x}, {self.y})"
-
-
-    
--- volodymyr-maksymiv/minesweeper 1.0/grid.py
+++ volodymyr-maksymiv/minesweeper 1.0/grid.py
@@ -5,10 +5,10 @@ import random
 class Grid:
 
     @staticmethod
-    def Grid_generate(location, grid_size):
+    def Grid_generate(location, grid_size, game): 
         for x in range(grid_size):
             for y in range(grid_size):
-                c = Cell(x , y)
+                c = Cell(x , y, game) 
                 c.create_btn_oblect(location)
                 c.cell_btn_object.grid(column=y, row=x)
 
@@ -18,7 +18,7 @@ class Grid:
             location,
             bg = 'black',
             fg = 'white',
-            text = f"Cells left:{Cell.cell_count}",
+            text = f"Cells left:...", 
             width = 14,
             height= 4,
             font = ("",30)
@@ -27,10 +27,14 @@ class Grid:
         Cell.cell_count_label_object = lbl
     
     @staticmethod
-    def randomize_mines(mines_count):
+    def randomize_mines(mines_count, first_clicked_cell):
+        
+        available_cells = [
+            cell for cell in Cell.all if cell != first_clicked_cell
+        ]
         
         picked_cells = random.sample(
-            Cell.all, mines_count
+            available_cells, mines_count
         )
         for picked_cell in picked_cells:
             picked_cell.is_mine = True
